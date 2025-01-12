4b852b15285738213725869cc43287b640922a75
mayowaibi <isaac.ibidun@gmail.com>
2025-01-11 22:36:25 -0500

    Added vercal analytics tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,6 @@
 import type { Metadata } from "next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
 import { Kanit, Bona_Nova } from "next/font/google";
 import "./globals.css";
 import { twMerge } from "tailwind-merge";
